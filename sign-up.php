bbefec9641dffb7c5b13e35c9ff51347a3e4fffd
staceeeybbb <sdballares.6@gmail.com>
2026-01-09 20:47:39 +0800

    Update sign-up.php

--- sign-up.php
+++ sign-up.php
@@ -213,5 +213,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             }
         }
     </script>
+      <footer class="py-6 text-center text-gray-400 text-xs">
+        &copy; <?php echo date('Y'); ?> CLA.I.RE System. All rights reserved.
+    </footer>
 </body>
 </html>
